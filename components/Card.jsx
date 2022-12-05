da7880556bd533a12894f32442d72104e6f45d1e
Dakota <davisdakota58@gmail.com>
2022-12-04 19:55:23 -0500

    card styling

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,3 +1,4 @@
+import { Rating } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -5,16 +6,12 @@ import Button from "./Button";
 import img from "/public/assets/product.jpg";
 
 function Card({ product, newArrivals }) {
-  const { id, slug, name, price, description } = product;
-  // hover:border-2 hover:border-primary
+  const { id, slug, name, price, description, reviews } = product;
+  const rating =
+    reviews.reduce((acc, curr) => acc + curr.rating, 0) / reviews.length;
+
   return (
-    <article
-      key={id}
-      className=""
-      // className={`transition-all border-2 border-transparent rounded ${
-      //   newArrivals ? "newArrivals" : ""
-      // }`}
-    >
+    <article key={id} className="">
       <div>
         <Link href={`/products/${slug}`}>
           <Image
@@ -28,7 +25,25 @@ function Card({ product, newArrivals }) {
           <div className="p-2 flex flex-col gap-1 items-start max-w-[18rem]">
             <h2 className="text-xl font-bold">{name}</h2>
             <p className="text-sm font-normal ">{description}</p>
-            <p className="text-lg font-medium">${price}</p>
+            <div className="flex justify-center items-center">
+              <Rating
+                className="text-primary"
+                name="read-only"
+                value={rating}
+                readOnly
+                precision={0.25}
+              />
+              {reviews.length > 0 && (
+                <p className="text-sm font-normal ml-2">({reviews.length})</p>
+              )}
+            </div>
+            <div className="flex items-center gap-4">
+              <p className="text-lg font-medium">${price}</p>
+              <p className="text-gray-500 text-sm">
+                + Shipping
+                {/* <del className="text-sm font-normal ml-2">$100</del> */}
+              </p>
+            </div>
           </div>
         </Link>
       </div>
