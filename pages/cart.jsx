a51597cb7abf17e664b577c27e2ae807b7dd67f9
Dakota <davisdakota58@gmail.com>
2023-01-07 16:18:18 -0500

    added summary to cart

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -8,6 +8,8 @@ import Button from "../components/utils/Button";
 import { formatToDollar } from "../utils/functions";
 
 function Cart() {
+  const tax = 0.05;
+  const shipping = 5;
   const data = useSiteData();
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
@@ -23,61 +25,103 @@ function Cart() {
         <link rel="icon" href="/assets/logo.png" />
       </Head>
       <h1 className="text-4xl p-6">Cart</h1>
-      <div className="grid grid-cols-1 gap-4">
-        {cart.cart.length > 0 ? (
-          cart.cart.map((item) => {
-            return (
-              <div key={item.id} className="grid grid-cols-5 gap-2">
-                <div className="max-w-[5rem]">
-                  <Image
-                    className="imageContain"
-                    alt={item.name}
-                    src={item.image}
-                    fill
-                  />
-                </div>
-                <div className="flex items-center">
-                  <h2>{item.name}</h2>
-                </div>
-                <div className="flex items-center">
-                  <h2>{formatToDollar(item.price)}</h2>
-                </div>
-                <div className="flex items-center">
-                  <h2>{item.quantity}</h2>
-                </div>
+      {/* items in cart */}
+      <div className="flex gap-20">
+        <div className="grid grid-cols-1 gap-4">
+          {cart.cart.length > 0 ? (
+            cart.cart.map((item) => {
+              return (
+                <div key={item.id} className="grid grid-cols-5 gap-2">
+                  <div className="max-w-[5rem]">
+                    <Image
+                      className="imageContain"
+                      alt={item.name}
+                      src={item.image}
+                      fill
+                    />
+                  </div>
+                  <div className="flex items-center">
+                    <h2>{item.name}</h2>
+                  </div>
+                  <div className="flex items-center">
+                    <h2>{formatToDollar(item.price)}</h2>
+                  </div>
+                  <div className="flex items-center">
+                    <h2>{item.quantity}</h2>
+                  </div>
 
-                <div className="flex gap-2 items-center">
-                  <Button
-                    className="bg-green-500"
-                    onClick={() => {
-                      dispatch(addToCart(item));
-                    }}
-                  >
-                    +
-                  </Button>
-                  <Button
-                    className="bg-blue-500"
-                    onClick={() => {
-                      dispatch(subFromCart(item));
-                    }}
-                  >
-                    -
-                  </Button>
-                  <Button
-                    className="bg-red-500"
-                    onClick={() => {
-                      dispatch(deleteItem(item));
-                    }}
-                  >
-                    X
-                  </Button>
+                  <div className="flex gap-2 items-center">
+                    <Button
+                      className="bg-green-500"
+                      onClick={() => {
+                        dispatch(addToCart(item));
+                      }}
+                    >
+                      +
+                    </Button>
+                    <Button
+                      className="bg-blue-500"
+                      onClick={() => {
+                        dispatch(subFromCart(item));
+                      }}
+                    >
+                      -
+                    </Button>
+                    <Button
+                      className="bg-red-500"
+                      onClick={() => {
+                        dispatch(deleteItem(item));
+                      }}
+                    >
+                      X
+                    </Button>
+                  </div>
                 </div>
-              </div>
-            );
-          })
-        ) : (
-          <h2>Cart is empty</h2>
-        )}
+              );
+            })
+          ) : (
+            <h2>Cart is empty</h2>
+          )}
+        </div>
+        {/* summary panel */}
+        <div>
+          <h2>Summary</h2>
+          {/* subtotal */}
+          <h2>
+            Subtotal:{" "}
+            {formatToDollar(
+              cart.cart.reduce((acc, item) => {
+                return acc + item.price * item.quantity;
+              }, 0)
+            )}
+          </h2>
+          {/* taxes */}
+          <h2>
+            Tax:{" "}
+            {formatToDollar(
+              cart.cart.reduce((acc, item) => {
+                return acc + item.price * item.quantity * tax;
+              }, 0)
+            )}
+          </h2>
+          {/* shipping */}
+          <h2>Shipping: {formatToDollar(shipping)}</h2>
+          {/* total */}
+          <h2>
+            Total:{" "}
+            {formatToDollar(
+              cart.cart.reduce((acc, item) => {
+                return (
+                  acc +
+                  item.price * item.quantity +
+                  item.price * item.quantity * tax
+                );
+              }, 0)
+            )}
+          </h2>
+
+          <Button primary>Checkout</Button>
+        </div>
       </div>
     </div>
   );
